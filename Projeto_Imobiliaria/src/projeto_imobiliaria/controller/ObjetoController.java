3a9a7116d391dfef401ec60ad4023a6e1a98e2d6
Thiago de Souza Ribeiro <thiagosr135@gmail.com>
2018-12-28 14:13:26 -0400

    Update nas interfaces de Cadastro e Exibição de Objeto. Implementação da função Salvar, Limpar e Buscar CEP do Objeto. 2812

--- Projeto_Imobiliaria/src/projeto_imobiliaria/controller/ObjetoController.java
+++ Projeto_Imobiliaria/src/projeto_imobiliaria/controller/ObjetoController.java
@@ -5,11 +5,20 @@
  */
 package projeto_imobiliaria.controller;
 
+import br.com.parg.viacep.ViaCEP;
+import br.com.parg.viacep.ViaCEPException;
+import br.eti.diegofonseca.MaskFieldUtil;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import projeto_imobiliaria.model.Objeto;
 
 /**
  * FXML Controller class
@@ -41,6 +50,72 @@ public class ObjetoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        MaskFieldUtil.cepField(txtCEP);
+        MaskFieldUtil.numericField(txtNumero);
+        MaskFieldUtil.numericField(txtCodigoUnico);
     }    
+
+    public void limpar(){
+        txtDescricao.setText("");
+        txtLogradouro.setText("");
+        txtBairro.setText("");
+        txtCidade.setText("");
+        txtCEP.setText("");
+        txtNumero.setText("");
+        txtCodigoUnico.setText("");
+        
+        txtDescricao.requestFocus();
+    }
+    
+    @FXML
+    private void salvar(ActionEvent event) {
+        
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("imobisoft");
+        EntityManager em = emf.createEntityManager();
+        Objeto objeto = new Objeto();
+        
+        objeto.setDescricao(txtDescricao.getText());
+        objeto.setLogradouro(txtLogradouro.getText());
+        objeto.setBairro(txtBairro.getText());
+        objeto.setCidade(txtCidade.getText());
+        objeto.setCEP(txtCEP.getText());
+        objeto.setNumero(txtNumero.getText());
+        objeto.setCodigoUnico(txtCodigoUnico.getText());
+        
+        em.getTransaction().begin();
+        em.persist(objeto);
+        em.getTransaction().commit();
+        limpar();
+        
+    }
+
+    @FXML
+    private void editar(ActionEvent event) {
+    }
+
+    @FXML
+    private void excluir(ActionEvent event) {
+    }
+
+    @FXML
+    private void buscarCEP(ActionEvent event) {
+        ViaCEP viaCep = new ViaCEP();
+
+        try {
+            viaCep.buscar(txtCEP.getText());
+
+            txtLogradouro.setText(viaCep.getLogradouro());
+            txtBairro.setText(viaCep.getBairro());
+            txtCidade.setText(viaCep.getLocalidade());
+            txtUF.setText(viaCep.getUf());
+            
+        } catch (ViaCEPException ex) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("ERRO");
+            alert.setHeaderText("Erro ao encontrar CEP!");
+            alert.setContentText("Verifique os campos de CEP e tente novamente");
+            alert.showAndWait();
+        }
+    }
     
 }
